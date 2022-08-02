794f429226435028ac0e19d38123da26ea5af12b
Jorowi <joni04.roman@gmail.com>
2022-08-02 22:31:52 +0200

    fillcorners

--- src/main/java/puzzle_cube_solver/Main.java
+++ src/main/java/puzzle_cube_solver/Main.java
@@ -1,29 +1,26 @@
 package puzzle_cube_solver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class Main {
     public static void main(String[] args) {
         App app = new App();
         //app.run();
-        generatePermutations(1,7);
+        generatePermutations((byte) 1, (byte) 7);
     }
-    public static void generatePermutations(int start, int end) {
-        List<Integer> edges = new ArrayList<>();
-        for (int i = start; i < end; i++) {
+    public static void generatePermutations(byte start, byte end) {
+        List<Byte> edges = new ArrayList<>();
+        for (byte i = start; i < end; i++) {
             edges.add(i);
             //System.out.println(Integer.toBinaryString(i));
         }
+        List<Piece> pers = new ArrayList<>();
+        heapPermutation(pers, edges, edges.size());
         List<Piece> pieces = new ArrayList<>();
-        heapPermutation(pieces, edges, edges.size());
-        System.out.println(pieces.size());
-        for (Piece piece : pieces) {
+        fillCorners(pers, pieces);
+        System.out.println(pers.size());
+        for (Piece piece : pers) {
             System.out.print(Integer.toBinaryString(piece.north) + " ");
             System.out.print(Integer.toBinaryString(piece.east) + " ");
             System.out.print(Integer.toBinaryString(piece.south) + " ");
@@ -32,22 +29,21 @@ public class Main {
         }
 
     }
-    public static void heapPermutation(List<Piece> pieces, List<Integer> edges, int size)
+    public static void heapPermutation(List<Piece> pers, List<Byte> edges, int size)
     {
         // if size becomes 1 then prints the obtained
         // permutation
         if (size == 1){
-
-            //pieces.add(new Piece(edges.get(0), edges.get(1), edges.get(2), edges.get(3)));
+            pers.add(new Piece(edges.get(0), edges.get(1), edges.get(2), edges.get(3)));
         }
 
         for (int i = 0; i < size; i++) {
 
-            heapPermutation(pieces, edges, size - 1);
+            heapPermutation(pers, edges, size - 1);
             // if size is odd, swap 0th i.e (first) and
             // (size-1)th i.e (last) element
             if (size % 2 == 1) {
-                int temp = edges.get(0);
+                byte temp = edges.get(0);
                 edges.set(0, edges.get(size - 1));
                 edges.set(size - 1, temp);
             }
@@ -55,7 +51,7 @@ public class Main {
             // If size is even, swap ith
             // and (size-1)th i.e last element
             else {
-                int temp = edges.get(i);
+                byte temp = edges.get(i);
                 edges.set(i, edges.get(size -1));
                 edges.set(size - 1, temp);
             }
@@ -63,7 +59,24 @@ public class Main {
         }
 
     }
-    public static  void fillCorners(List<Piece> pieces) {
-
+    public static  void fillCorners(List<Piece> pers, List<Piece> pieces) {
+        for(Piece piece : pers) {
+            pieces.add(new Piece((byte) (piece.north << 1), (byte) (piece.east << 1), (byte) (piece.south << 1), (byte) (piece.west << 1)));
+        }
+        for(Piece piece : pieces) {
+            boolean uL, uR, bR, bL = false;
+            if(MathHelper.isBitOne(piece.west, 2) || MathHelper.isBitOne(piece.north, 4)) {
+                uL = true;
+            }
+            if(MathHelper.isBitOne(piece.north, 2) || MathHelper.isBitOne(piece.east, 4)) {
+                uR = true;
+            }
+            if(MathHelper.isBitOne(piece.east, 2) || MathHelper.isBitOne(piece.south, 4)) {
+                bR = true;
+            }
+            if(MathHelper.isBitOne(piece.south, 2) || MathHelper.isBitOne(piece.west, 4)) {
+                bL = true;
+            }
+        }
     }
 }
